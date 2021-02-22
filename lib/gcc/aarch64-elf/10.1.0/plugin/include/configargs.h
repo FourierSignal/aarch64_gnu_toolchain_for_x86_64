7c336ed062e23916f15cdd15ddd8d1d6dd1fcf4b
FourierSignal <sudheer.linx@gmail.com>
2021-02-23 04:03:20 +0530

    aarch64-elf toolchain

---
+++ lib/gcc/aarch64-elf/10.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-10.1.0/configure --target=aarch64-elf --prefix=/home/jaguar/Jaguar_workspace/Embedded/RSP3_revB/toolchains/gcc_from_GNU/bare-metal-gcc-build/aarch64-elf --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
